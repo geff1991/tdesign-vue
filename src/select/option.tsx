e898761b96496b4ac31de07253ebf0ad587aad74
geff1991 <geff1991@yeah.net>
2022-05-04 15:25:48 +0800

    feat(select): composition api

--- src/select/option.tsx
+++ src/select/option.tsx
@@ -1,8 +1,18 @@
-import Vue, { VNode } from 'vue';
+import {
+  computed,
+  ref,
+  SetupContext,
+  toRefs,
+  watch,
+  onMounted,
+  inject,
+  onBeforeUnmount,
+  defineComponent,
+} from '@vue/composition-api';
+import Vue from 'vue';
 import { ScopedSlotReturnValue } from 'vue/types/vnode';
 import get from 'lodash/get';
 import { renderContent } from '../utils/render-tnode';
-import mixins from '../utils/mixins';
 import { scrollSelectedIntoView } from '../utils/dom';
 import { prefix } from '../config';
 import CLASSNAMES from '../utils/classnames';
@@ -11,149 +21,129 @@ import props from './option-props';
 import { TdOptionProps } from './type';
 import Checkbox from '../checkbox/index';
 import { SelectInstance } from './instance';
-import { ClassName } from '../common';
-import { getKeepAnimationMixins } from '../config-provider/config-receiver';
-
-const keepAnimationMixins = getKeepAnimationMixins<OptionInstance>();
 
 const selectName = `${prefix}-select`;
 export interface OptionInstance extends Vue {
   tSelect: SelectInstance;
 }
 
-export default mixins(keepAnimationMixins).extend({
+export default defineComponent({
   name: 'TOption',
-  data() {
-    return {
-      isHover: false,
-      formDisabled: undefined,
-    };
-  },
   props: { ...props },
   components: {
     TCheckbox: Checkbox,
   },
   directives: { ripple },
-  inject: {
-    tSelect: {
-      default: undefined,
-    },
-  },
-  watch: {
-    value() {
-      this.tSelect && this.tSelect.getOptions(this);
-    },
-    label() {
-      this.tSelect && this.tSelect.getOptions(this);
-    },
-    hovering() {
-      if (this.hovering) {
+  setup(props: TdOptionProps, context: SetupContext) {
+    const isHover = ref(false);
+    const formDisabled = ref(undefined);
+    const { value, label, disabled } = toRefs(props);
+
+    const tSelect: any = inject('tSelect');
+    watch(value, () => {
+      tSelect && tSelect.test;
+      tSelect && tSelect.getOptions(this);
+    });
+    watch(label, () => {
+      tSelect && tSelect.getOptions(this);
+    });
+    const tDisabled = computed(() => formDisabled.value || disabled.value);
+    const hovering = computed(() => (
+      tSelect
+        && tSelect.visible
+        && tSelect.hoverOptions[tSelect.hoverIndex]
+        && tSelect.hoverOptions[tSelect.hoverIndex][tSelect.realValue] === value.value
+    ));
+    watch(hovering, (val) => {
+      if (val) {
         const timer = setTimeout(() => {
-          scrollSelectedIntoView(this.tSelect.getOverlayElm(), this.$el as HTMLElement);
+          scrollSelectedIntoView(tSelect.getOverlayElm(), context.root.$el as HTMLElement);
           clearTimeout(timer);
-        }, this.tSelect.popupOpenTime); // 待popup弹出后再滚动到对应位置
+        }, tSelect.popupOpenTime); // 待popup弹出后再滚动到对应位置
       }
-    },
-  },
-  computed: {
-    tDisabled(): boolean {
-      return this.formDisabled || this.disabled;
-    },
-    // 键盘上下按键选中hover样式的选项
-    hovering(): boolean {
-      return (
-        this.tSelect
-        && this.tSelect.visible
-        && this.tSelect.hoverOptions[this.tSelect.hoverIndex]
-        && this.tSelect.hoverOptions[this.tSelect.hoverIndex][this.tSelect.realValue] === this.value
-      );
-    },
-    multiLimitDisabled(): boolean {
-      if (this.tSelect && this.tSelect.multiple && this.tSelect.max) {
+    });
+    const multiLimitDisabled = computed(() => {
+      if (tSelect && tSelect.multiple && tSelect.max) {
         if (
-          this.tSelect.value instanceof Array
-          && this.tSelect.value.indexOf(this.value) === -1
-          && this.tSelect.max <= this.tSelect.value.length
+          tSelect.value instanceof Array
+          && tSelect.value.indexOf(value.value) === -1
+          && tSelect.max <= tSelect.value.length
         ) {
           return true;
         }
       }
       return false;
-    },
-    classes(): ClassName {
-      return [
-        `${prefix}-select-option`,
-        {
-          [CLASSNAMES.STATUS.disabled]: this.tDisabled || this.multiLimitDisabled,
-          [CLASSNAMES.STATUS.selected]: this.selected,
-          [CLASSNAMES.SIZE[this.tSelect && this.tSelect.size]]: this.tSelect && this.tSelect.size,
-          [`${prefix}-select-option__hover`]: this.hovering,
-        },
-      ];
-    },
-    isCreatedOption(): boolean {
-      return this.tSelect.creatable && this.value === this.tSelect.searchInput;
-    },
-    show(): boolean {
+    });
+    const classes = computed(() => [
+      `${prefix}-select-option`,
+      {
+        [CLASSNAMES.STATUS.disabled]: tDisabled || multiLimitDisabled,
+        [CLASSNAMES.STATUS.selected]: selected.value,
+        [CLASSNAMES.SIZE[tSelect && tSelect.size]]: tSelect && tSelect.size,
+        [`${prefix}-select-option__hover`]: hovering.value,
+      },
+    ]);
+    const isCreatedOption = computed(() => tSelect.creatable && value.value === tSelect.searchInput);
+    const show = computed(() => {
       /**
        * 此属性主要用于slots生成options时显示控制，直传options由select进行显示控制
        * create的option，始终显示
        * canFilter只显示待匹配的选项
        */
-      if (!this.tSelect) return false;
-      if (this.isCreatedOption) return true;
-      if (this.tSelect.canFilter && this.tSelect.searchInput !== '') {
-        return this.tSelect.filterOptions.some(
-          (option: TdOptionProps) => get(option, this.tSelect.realValue) === this.value,
-        );
+      if (!tSelect) return false;
+      if (isCreatedOption.value) return true;
+      if (tSelect.canFilter && tSelect.searchInput !== '') {
+        return tSelect.filterOptions.some((option: TdOptionProps) => get(option, tSelect.realValue) === value.value);
       }
       return true;
-    },
-    labelText(): string | number {
-      return this.label || this.value;
-    },
-    selected(): boolean {
+    });
+    const labelText = computed(() => label.value || value.value);
+    const selected = computed(() => {
       let flag = false;
-      if (!this.tSelect) return false;
-      if (this.tSelect.value instanceof Array) {
-        if (this.tSelect.labelInValue) {
-          flag = this.tSelect.value.map((item) => get(item, this.tSelect.realValue)).indexOf(this.value) !== -1;
+      if (!tSelect) return false;
+      if (tSelect.value instanceof Array) {
+        if (tSelect.labelInValue) {
+          flag = tSelect.value.map((item: any) => get(item, tSelect.realValue)).indexOf(value.value) !== -1;
         } else {
-          flag = this.tSelect.value.indexOf(this.value) !== -1;
+          flag = tSelect.value.indexOf(value.value) !== -1;
         }
-      } else if (typeof this.tSelect.value === 'object') {
-        flag = get(this.tSelect.value, this.tSelect.realValue) === this.value;
+      } else if (typeof tSelect.value === 'object') {
+        flag = get(tSelect.value, tSelect.realValue) === value.value;
       } else {
-        flag = this.tSelect.value === this.value;
+        flag = tSelect.value === value.value;
       }
       return flag;
-    },
-  },
-  methods: {
-    select(e: MouseEvent | KeyboardEvent) {
+    });
+    const select = (e: MouseEvent | KeyboardEvent) => {
       e.stopPropagation();
-      if (this.tDisabled || this.multiLimitDisabled) {
+      if (tDisabled.value || multiLimitDisabled.value) {
         return false;
       }
-      const parent = this.$el.parentNode as HTMLElement;
+      const parent = context.root.$el.parentNode as HTMLElement;
       if (parent && parent.className.indexOf(`${selectName}__create-option`) !== -1) {
-        this.tSelect && this.tSelect.createOption(this.value.toString());
+        tSelect && tSelect.createOption(value.value.toString());
       }
-      this.tSelect && this.tSelect.onOptionClick(this.value, e);
-    },
-    mouseEvent(v: boolean) {
-      this.isHover = v;
-    },
+      tSelect && tSelect.onOptionClick(value.value, e);
+    };
+    const mouseEvent = (v: boolean) => {
+      isHover.value = v;
+    };
+    onMounted(() => {
+      tSelect && tSelect.getOptions(this);
+    });
+    onBeforeUnmount(() => {
+      tSelect && tSelect.hasSlotOptions && tSelect.destroyOptions(this);
+    });
+    return {
+      mouseEvent,
+      select,
+      classes,
+      tSelect,
+    };
   },
-  mounted() {
-    this.tSelect && this.tSelect.getOptions(this);
-  },
-  beforeDestroy() {
-    this.tSelect && this.tSelect.hasSlotOptions && this.tSelect.destroyOptions(this);
-  },
-  render(): VNode {
+  render() {
     const {
-      classes, labelText, selected, disabled, multiLimitDisabled, show,
+      classes, labelText, selected, disabled, multiLimitDisabled, show, tSelect,
     } = this;
     const children: ScopedSlotReturnValue = renderContent(this, 'default', 'content');
     const optionChild = children || labelText;
@@ -166,7 +156,7 @@ export default mixins(keepAnimationMixins).extend({
         onClick={this.select}
         v-ripple={this.keepAnimation.ripple}
       >
-        {this.tSelect && this.tSelect.multiple ? (
+        {tSelect && tSelect.multiple ? (
           <t-checkbox
             checked={selected}
             disabled={disabled || multiLimitDisabled}
